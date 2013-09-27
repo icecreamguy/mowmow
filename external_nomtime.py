5337a10b19813208b95c2c648b2febf7e91e42d2
Julius Schlosburg <icecreamguy@gmail.com>
2013-09-26 17:05:24 -0700

    Fixed external_nomtime script

--- external_nomtime.py
+++ external_nomtime.py
@@ -1,19 +1,18 @@
 import sys
 import os
 os.chdir('/var/www/mowmow/')
-sys.path.append('/var/www/mowmow/')
-sys.path.append('/var/www/mowmow/mowlib/')
+sys.path.append('/var/www/mowmow/api')
+sys.path.append('/var/www/mowmow/api/mowlib/')
 import mow_utils
 import config
 from web import utils
 
 db = config.db
-schedule = mow_utils.schedule()
-status = mow_utils.get_status(db, schedule)
 
-if status['fed'] == 1:
+print(status)
+if status['fed']:
     print('Already fed')
 else:
     print('Feeding')
     data = utils.storage(feed = 1)
-    mow_utils.feed_cycle(data, schedule, mow_utils.date_strings())
+    mow_utils.feed_cycle(data, mow_utils.date_strings(), None, external=True)
